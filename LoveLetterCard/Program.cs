a40d166866b75b6e9b933925bc1183b401422eee
Ibrahim Khan <M.KHAN-2020@hull.ac.uk>
2021-08-08 09:07:27 +0100

    Add project files.

---
+++ LoveLetterCard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoveLetterCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
